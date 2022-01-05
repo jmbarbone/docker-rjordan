8769eb3ac22b42c8f0000e6e28e1ff20f9605279
jmbarbone <jmbarbone@gmail.com>
2022-01-05 12:48:27 -0500

    corrects tagging

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@ docker build --tag rjordan .
 # docker run -p 8080:80 -d --name pyr pyr
 # docker run -it --name pyr pyr
 
-docker tag pyr jmbarbone/rjordan
+docker rjordan pyr jmbarbone/rjordan
 docker push jmbarbone/rjordan
