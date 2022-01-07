5fd502a9d0c478e7f4ef6ccc72a8a83c441e69fe
jmbarbone <jmbarbone@gmail.com>
2022-01-07 12:23:42 -0500

    don't confuse the names

--- docker.sh
+++ docker.sh
@@ -1,6 +1,4 @@
 docker build --tag rjordan .
-# docker run -p 8080:80 -d --name pyr pyr
-# docker run -it --name pyr pyr
 
-docker rjordan pyr jmbarbone/rjordan
+docker tag rjordan jmbarbone/rjordan
 docker push jmbarbone/rjordan
