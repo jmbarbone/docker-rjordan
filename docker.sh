dbba6edd9a5611f9ed4572d94afa5a4c3d11526c
Jordan Mark Barbone <jbarbone@cogstate.com>
2023-06-30 16:41:41 -0400

    formatting

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 dir=${1:-.}
-docker build --tag rjordan $dir
-docker tag rjordan jmbarbone/rjordan
-docker push jmbarbone/rjordan
+docker build --tag rjordan $dir && \
+    docker tag rjordan jmbarbone/rjordan && \
+    docker push jmbarbone/rjordan
